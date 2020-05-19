16138880348344d7c97b7e211b73ca8a6f5dd2b8
yunwhite <s2018s12@e-mirim.hs.kr>
2020-05-20 05:09:19 +0900

    초기 커밋.

---
+++ studentlist/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace studentlist
+{
+    class Program
+    {
+        class Teacher
+        {
+            public string name;
+            public string major;
+        }
+        static void Main(string[] args)
+        {
+            List<Teacher> list = new List<Teacher>();
+            list.Add(new Teacher() { name = "함기훈", major = "정보컴퓨터" });
+            list.Add(new Teacher() { name = "박성래", major = "수학" });
+            list.Add(new Teacher() { name = "민주리", major = "정보컴퓨터" });
+            list.Add(new Teacher() { name = "박지우", major = "정보컴퓨터" });
+            list.Add(new Teacher() { name = "김보경", major = "가정" });
+            list.Add(new Teacher() { name = "장민주", major = "디자인" });
+
+            for (int i = list.Count - 1; i >= 0; i--)
+            {
+                if(list[i].major == "정보컴퓨터")
+                {
+                    list.RemoveAt(i);
+                }
+            }
+            foreach(var item in list)
+            {
+                Console.WriteLine(item.name + "쌤 : " + item.major);
+            }
+        }
+    }
+}
---
+++ studentlist/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace studentlist
+{
+    class Student
+    {
+        public string id;
+        public string name;
+        public int grade;
+        public string major;
+        public DateTime birthday;
+    }
+}
